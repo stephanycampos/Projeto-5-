059449974bd34da2798df539569e629ebc48deb4
Stephany <stephanyenne@gmail.com>
2025-12-15 19:05:10 -0300

    Projeto lista de contatos com React, Redux e Styled Components

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,7 +2,7 @@ export const colors = {
   primary: '#322e46',
   secondary: '#6472a1',
   offWhite: '#fffefb',
-  tertiary: '#eef3ff',
-  emphasis: '#4e8088',
-  alertPrimary: '#9a5151',
+  tertiary: '#dde1ecff',
+  emphasis: '#69ee58ff',
+  alertPrimary: '#ea0f0fff',
 }
